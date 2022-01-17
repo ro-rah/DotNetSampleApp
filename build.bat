f4dbbfbdbee82fab013211e576328c5e245862df
ronak <ronak@example.com>
2022-01-17 13:55:54 -0600

    update for local install

--- build.bat
+++ build.bat
@@ -2,12 +2,12 @@
 echo Deleting existing build session Id file
 @del .\buildSession
 
-set SL_TOOLS_PATH=..\SL.DotNet-2.0.1.741\x64\
+set SL_TOOLS_PATH=C:\bin\SL.DotNet-2.0.1.749\x64\
 set SL_AGENT="%SL_TOOLS_PATH%sl.dotnet.exe"
 
 set SL_PROXY=http://127.0.0.1:8888
-set SL_APP_NAME=KiskassDemo
-set SL_BRANCH_NAME=Branch1
+set SL_APP_NAME=BadassDemo
+set SL_BRANCH_NAME=master
 
 @rem GENERATE A UNIQUE BUILD NAME BASED ON THE CURRENT TIME. See http://www.larmib.com/2011/dos-command-to-get-the-2-digit-hour-minute-and-second/ and https://stackoverflow.com/questions/4248220/how-can-i-retrieve-the-seconds-part-of-time-in-cmd
 For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c%%a%%b)
@@ -20,5 +20,5 @@ pause
 @rem Prepare for MSBuild
 %SL_AGENT% prepareForMsBuild --TokenFile "%cd%\\sealights.token" --buildSessionIdFile buildSession --baseDir "%cd%" --workspacePath "%cd%" --proxy %SL_PROXY% --ignoreGeneratedCode true --logEnabled true --ignoreCertificateErrors true --excludedProjects *NUnit*,*XUnit*,*Tests*,*WebApplication*
 @rem Run MSBuild
-"%PROGRAMFILES(x86)%\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\MSBuild.exe" DotNetSampleApp.sln /t:Rebuild
+"%PROGRAMFILES(x86)%\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" DotNetSampleApp.sln /t:Rebuild
 @rem del after.DotNetSampleApp.sln.targets
